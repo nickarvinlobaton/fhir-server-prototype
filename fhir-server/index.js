447cf08690c155131a9bbdd045dfbf3f85907c44
Nick Lobaton <nickm3gtr@gmail.com>
2020-09-23 11:03:20 +0800

    added auth for the express routes on fhir-server

--- fhir-server/index.js
+++ fhir-server/index.js
@@ -3,18 +3,30 @@ const express = require("express");
 
 const config = require("./config");
 
+require("dotenv").config();
+
+// AUTH for express routes, same as FHIR server
+// Integrate it into your routes via middleware or some other mechanism
+// Our strategy is a bearer strategy so use bearer for the name
+const passport = require("passport");
+let name = "bearer";
+let options = { session: false };
+
+// Initialize Express
+let app = express();
+
+// Add router to express app
+app.use(
+  "/api/agate",
+  passport.authenticate(name, options),
+  require("./routes/agate")
+);
+
 const {
   initialize,
   loggers,
   constants,
 } = require("@asymmetrik/node-fhir-server-core");
-
-require("dotenv").config();
-
-let app = express();
-
-// Add router to express app
-app.use("/api/agate", require("./routes/agate"));
 
 let server = initialize(config, app);
 let logger = loggers.get("default");
